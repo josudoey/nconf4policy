0a386e24e0128414136814c09743bbe44ba956f2
Joey Fang <josudoey@gmail.com>
2017-02-11 23:15:02 +0800

    [modify] patch code for eslint

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 var nconf = require('nconf');
-var merge = require("merge");
+var merge = require('merge');
 var _defaults = nconf.defaults.bind(nconf);
 var _overrides = nconf.overrides.bind(nconf);
-
 var checkDuplicate = function (dst, src) {
   Object.keys(src).forEach(function (key) {
     if (key in dst) {
-      throw new Error("duplicate configure key.");
+      throw new Error('duplicate configure key.');
     }
   });
 };
-
 var defaults = function (options) {
   options = options || {};
   checkDuplicate(nconf.stores.defaults.store, options);
-  merge(nconf.stores.defaults.store, options)
-}
-
+  merge(nconf.stores.defaults.store, options);
+};
 var overrides = function (options) {
   options = options || {};
   checkDuplicate(nconf.stores.overrides.store, options);
-  merge(nconf.stores.overrides.store, options)
-}
-
+  merge(nconf.stores.overrides.store, options);
+};
 var patch = function () {
-  if (typeof nconf.stores.overrides === "undefined") {
+  if (typeof nconf.stores.overrides === 'undefined') {
     _overrides();
     nconf.stores.overrides.readOnly = false;
   }
-  if (typeof nconf.stores.defaults === "undefined") {
-    _defaults()
+  if (typeof nconf.stores.defaults === 'undefined') {
+    _defaults();
   }
-  if (typeof nconf.stores.file === "undefined") {
+  if (typeof nconf.stores.file === 'undefined') {
     nconf.stores.file = nconf.stores.overrides;
   }
-
-  //sort file > overrides > defaults
   var tmp = merge(true, nconf.stores);
   Object.keys(nconf.stores).forEach(function (name) {
     delete nconf.stores[name];
@@ -44,9 +38,8 @@ var patch = function () {
   nconf.stores.overrides = tmp.overrides;
   nconf.stores.defaults = tmp.defaults;
   merge(nconf.stores, tmp);
-}
+};
 patch();
-
 nconf.defaults = defaults;
 nconf.overrides = overrides;
 module.exports = nconf;
